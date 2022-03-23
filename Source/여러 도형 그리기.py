5ab4534fb35c36d03ff8c3d938ca15540b967b9b
JiWon <radiohead@knou.ac.kr>
2022-03-23 11:29:31 +0900

    Create 여러 도형 그리기.py
    turtle 이용하여 선, 원, 텍스트, 점, 사각형 그리기

---
+++ Source/여러 도형 그리기.py
@@ -0,0 +1,42 @@
+import turtle
+
+def drawLine(x1, y1, x2, y2):
+    turtle.penup()
+    turtle.goto(x1, y1)
+    turtle.pendown()
+    turtle.goto(x2, y2)
+
+def circle(x, y, radius):
+    turtle.penup()
+    turtle.goto(x, y - radius)
+    turtle.pendown()
+    turtle.circle(radius)
+
+def writeText(s, x, y):
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.pendown()
+    turtle.write(s)
+
+def drawPoint(x, y):
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.pendown()
+    turtle.circle(3)
+    turtle.end_fill()
+
+def drawRectangle(x, y, width, height):
+    turte.penup()
+    turtle.goto(x + width / 2, y + height / 2)
+    turtle.pendown()
+    turtle.right(90)
+    turtle.forward(width)
+    turtle.right(90)
+    turtle.forward(height)
+    turtle.right(90)
+    turtle.forward(width)
+
+    
+
+    
+    
